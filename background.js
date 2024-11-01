96af4dbe18ab6f3abc8d7e145b281509cc66bfa6
Melissa Code <meliacode@users.noreply.github.com>
2024-11-01 16:31:59 -0400

    chore: update background to avoid overriding options on update

--- background.js
+++ background.js
@@ -4,11 +4,39 @@
  */
 
 chrome.runtime.onInstalled.addListener(({ reason, previousVersion }) => {
-    // Set the default upon installation
-    chrome.storage.sync.set({
-        // Video Length
-        doFadeByLength: true, // Default: enable filter by video length
-        videoLengthMin: 0, // Default: minimum length 0 minutes
-        videoLengthMax: 30, // Default: maximum length 30 minutes
-    });
+    /**
+     * If the extension is installed
+     */
+
+    if (reason === "install") {
+        // Set the default upon installation
+        chrome.storage.sync.set({
+            // Video Length
+            doFadeByLength: true, // Default: enable filter by video length
+            videoLengthMin: 0, // Default: minimum length 0 minutes
+            videoLengthMax: 30, // Default: maximum length 30 minutes
+        });
+
+        // Open the options page
+        chrome.runtime.openOptionsPage();
+    }
+
+    /**
+     * If the extension is updated
+     */
+
+    if (reason === "update") {
+        // Get the current version
+        const currentVersion = chrome.runtime.getManifest().version;
+
+        if (previousVersion === currentVersion) {
+            return;
+        }
+
+        // Log the update
+        console.log(`[SimpleTube] Updated from ${previousVersion} to ${currentVersion}`);
+
+        // Open the options page
+        chrome.runtime.openOptionsPage();
+    }
 });
