ba7840becf858f49cbb3b3775429d2f47eb0936b
Melissa Code <meliacode@users.noreply.github.com>
2024-11-26 19:34:09 -0500

    chore: adding linting and code formating rules

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default [
+    ...compat.extends('eslint:recommended'),
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                chrome: 'readonly',
+                browser: 'readonly',
+            },
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        rules: {
+            indent: ['error', 4],
+            'linebreak-style': ['error', 'unix'],
+            quotes: ['error', 'single'],
+            semi: ['error', 'always'],
+        },
+    },
+];
